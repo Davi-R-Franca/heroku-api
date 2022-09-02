2681b5f0605396e50542d534ae87e4de1e6d1f1d
LSE <davifranca2000@gmail.com>
2022-09-01 21:43:39 -0400

    inserindo banco de dados

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from .database import conn
 
 app = FastAPI()
 
 @app.get('/')
 async def retorna_algo():
     return {"Olá":"Mundo"}
+
+@app.post('/cria')
+async def cria_usuario(user):
+    idt = conn.local.user.insert_one(user).inserted_id
+    usuario = conn.local.user.find_one({"_id":idt})
+    return usuario
